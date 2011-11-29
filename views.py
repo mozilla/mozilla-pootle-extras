911b49a7cc571e43b23da29663b2e6a1627e82e0
Peter Bengtsson <mail@peterbe.com>
2011-11-28 16:47:59 -0800

    optimized the contributors view

--- views.py
+++ views.py
@@ -42,12 +42,12 @@ def verbatim_contributors(request,
     def lmap(id):
         return _language_map[id]
 
+    EXCLUDED_PROJECT_NAMES = (
+      u"Testing, please don't work here",
+    )
     _project_map = {}
-    for project in Project.objects.exclude(fullname=u"Testing, please don't work here"):
-        if 'test' in project.fullname.lower():
-            print "PARNING"
-            print repr(project.fullname)
-            print
+
+    for project in Project.objects.exclude(fullname__in=EXCLUDED_PROJECT_NAMES):
         _project_map[project.id] = project.fullname
 
     def pmap(id):
@@ -66,6 +66,7 @@ def verbatim_contributors(request,
             print repr(user.username)
             print
         return name
+
 
     for profile in PootleProfile.objects.all().select_related('user'):#order_by('?')[:10]:
         name = user_name(profile.user)
@@ -91,15 +92,6 @@ def verbatim_contributors(request,
             except Project.DoesNotExist:
                 pass
 
-        #print "\t", profile.suggester.all()
-
-        #if profile.suggester.all().count():
-        #    print dir(profile.suggester.all()[0])
-
-        #print
-    #print _projects
-    #print _languages
-#    pprint(_languages)
     contributors = []
     for language, projectsmap in _languages.items():
         projects = []
@@ -110,90 +102,124 @@ def verbatim_contributors(request,
         if users:
             contributors.append((language, projects))
     contributors.sort()
-#    pprint(contributors)
 
 
-#    for i, p in enumerate(_projects):
-#        try:
-#            pn=Project.objects.get(id=p).fullname
-#        except Project.DoesNotExist:
-#            pn='*deleted*'
-#        print str(i+1).ljust(2), pn.ljust(40), len(_projects[p])
-
-#    for i, l in enumerate(_languages):
-#        #try:
-#        ln=Language.objects.get(id=l).fullname
-#        #except Language.DoesNotExist:
-#        #    ln='*deleted*'
-
-#        print str(i+1).ljust(2), ln.ljust(40), len(_languages[l])
+    print "# NODES", sum(len(users) for language, projects in contributors for name, users in projects)
+    return render_to_response(template_name,
+                              {'contributors': contributors},
+                              context_instance=RequestContext(request))
 
 
+def verbatim_contributors(request,
+              template_name='mozilla_extras/verbatim-contributors.html'):
+    """render a nested list like this::
 
-    if 0:#for profile in PootleProfile.objects.all().order_by('?')[:1]:
-#        print repr(profile.user)
-        langs = [x.fullname for x in profile.languages.all()]
-#        print "\tLs:", langs
-        #lang_counts[profile.languages.all().count()] += 1
-        projs = [x.fullname for x in profile.projects.all()]
-#        print "\tPs:", projs
-        #proj_counts[profile.projects.all().count()] += 1
+        contributors = [
+          ('french', [
+            ('Hackaraus', ['Auser Name', 'username2', ...]),
+            ('Project 2', ['username1', 'usernameX', ...]),
+            ...
+            ]),
+          ('spanish', [
+            ('Project 1', ['User 1', 'User2', ...]),
+            ('Project 2', ['User 1', 'UserX', ...]),
+            ]
 
-        langs2 =set()
-        projs2=set()
-        for s in Submission.objects.filter(submitter=profile).select_related('translation_project'):
-            tp = s.translation_project
-            #print dir(tp)
-            #print type(tp.language), type(tp.language_id)
-            #break
-            langs2.add(tp.language_id)
+        }
+    """
+    EXCLUDED_PROJECT_NAMES = (
+      u"Testing, please don't work here",
+    )
+
+    EXCLUDED_USERNAMES = (
+      u'sorryjusttesting',
+      u'marcoos_test',
+      u'test',
+      u'\'">,test \'">,test',
+      u'testo\'"><',
+    )
+
+    from pootle_statistics.models import Submission
+    from pootle_translationproject.models import TranslationProject
+    from pootle_project.models import Project
+    from pootle_language.models import Language
+    from pootle_app.models import Suggestion
+
+    _user_names = {}  # user id -> name
+    for user in (User.objects.exclude(username__in=EXCLUDED_USERNAMES)
+                 .values('id', 'first_name', 'last_name', 'username')):
+        name = ('%s %s' % (user['first_name'], user['last_name'])).strip()
+        if not name:
+            name = user['username']
+        #if 'test' in name:
+        #    print "WARNING"
+        #    print repr(name)
+        #    print repr(user['username'])
+        #    print
+        _user_names[user['id']] = name
+
+    _language_names = {}  # language id -> name
+    for language in Language.objects.all().values('id', 'fullname'):
+        _language_names[language['id']] = language['fullname']
+
+    _project_names = {}  # project id -> name
+    for project in (Project.objects
+                    .exclude(fullname__in=EXCLUDED_PROJECT_NAMES)
+                    .values('id', 'fullname')):
+        _project_names[project['id']] = project['fullname']
+
+    # map users to projects per language across:
+    # submitters, suggesters and reviewers
+    _languages = {}
+    _tp_to_lang_id = {}
+    _tp_to_proj_id = {}
+
+    # prepare a map of TranslationProject IDs to
+    # language and project to save queries for later
+    for tp in (TranslationProject.objects.all()
+               .values('id', 'language_id', 'project_id')):
+        _tp_to_lang_id[tp['id']] = tp['language_id']
+        _tp_to_proj_id[tp['id']] = tp['project_id']
+
+    for model, user_key in ((Submission, 'submitter_id'),
+                            (Suggestion, 'suggester_id'),
+                            (Suggestion, 'reviewer_id')):
+        for item in (model.objects.all()
+                     .values('translation_project_id', user_key)
+                     .distinct()):
+            lang_id = _tp_to_lang_id[item['translation_project_id']]
+            proj_id = _tp_to_proj_id[item['translation_project_id']]
+            user_id = item[user_key]
+            if not user_id:  # bad paste on_delete cascades
+                continue
+            if lang_id not in _languages:
+                _languages[lang_id] = {}
+            if proj_id not in _languages[lang_id]:
+                _languages[lang_id][proj_id] = set()
+            _languages[lang_id][proj_id].add(user_id)
+
+    # finally, turn this massive dict into a list of lists of lists
+    # to be used in the template to loop over.
+    # also change from IDs to real names
+    contributors = []
+    for lang_id, projectsmap in _languages.items():
+        language = _language_names[lang_id]
+        projects = []
+        users = None
+        for proj_id, user_ids in projectsmap.items():
+            usersset = [_user_names[x] for x in user_ids]
+            users = sorted(usersset)
             try:
-                projs2.add(tp.project_id)
-            except Project.DoesNotExist:
-                pass
-        for s in Suggestion.objects.filter(suggester=profile).select_related('translation_project'):
-            tp = s.translation_project
-            langs2.add(tp.language_id)
-            try:
-                projs2.add(tp.project_id)
-            except Project.DoesNotExist:
-                pass
-        #for r in Review.objects.filter(suggester=profile).select_related('translation_project'):
-        #    tp = s.translation_project
-        #    langs2.add(tp.language_id)
-        #    try:
-        #        projs2.add(tp.project_id)
-        #    except Project.DoesNotExist:
-        #        pass
-
-        lang_counts[len(langs2)] += 1
-        proj_counts[len(projs2)] += 1
-
-#        print "\tL2s:", langs2
-#        print "\tP2s:", projs2
-#        print
-
-    if 0:
-
-        print "LANGS"
-        lang_counts =dict(lang_counts)
-        pprint(lang_counts)
-
-        people = sum(lang_counts.values()) * 1.0
-        for c in sorted(lang_counts.keys()):
-            print c, "%.1f%%"%(lang_counts[c]*100 / people)
-
-
-        print "PROJS"
-        proj_counts=dict(proj_counts)
-        pprint(proj_counts)
-
-        peoplex = sum(lang_counts.values()) * 1.0
-        assert peoplex==people
-        for c in sorted(proj_counts.keys()):
-            print c, "%.1f%%"%(proj_counts[c]*100 / people)
-
-
+                projectname = _project_names[proj_id]
+            except KeyError:
+                # some legacy broken project or excluded
+                continue
+            if users:
+                projects.append((projectname, users))
+        if projects:
+            contributors.append((language, projects))
+    contributors.sort()
+#    print "# NODES", sum(len(users) for language, projects in contributors for name, users in projects)
     return render_to_response(template_name,
                               {'contributors': contributors},
                               context_instance=RequestContext(request))
